9534f236a7b4909618d1fbd1863e19c20907c350
234189@student.uek.krakow.pl <234189@student.uek.krakow.pl>
2024-05-22 02:56:14 +0200

    testing interface

--- plainsimpletodoapp/main.py
+++ plainsimpletodoapp/main.py
@@ -2,7 +2,27 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    def add_clicked(e):
+        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        view.update()
 
+    new_task = ft.TextField(hint_text="What's needs to be done?", expand=True)
+    tasks_view = ft.Column()
+    view=ft.Column(
+        width=600,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked),
+                ],
+            ),
+            tasks_view,
+        ],
+    )
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
 ft.app(main)
